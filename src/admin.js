a4a41bae1a100e9a8e6d4f0c7e596135ffc95d9b
c-kendrick <ckendrick.dev@gmail.com>
2026-07-12 16:41:11 +1000

    Add authenticated admin profile controls

---
+++ src/admin.js
@@ -0,0 +1,17 @@
+import { supabaseRequest } from './supabase.js';
+
+function headers(accessToken) {
+  return { Authorization: 'Bearer ' + accessToken, 'Content-Type': 'application/json' };
+}
+
+export function listProfiles(accessToken) {
+  return supabaseRequest('/rest/v1/profiles?select=id,username,display_name,approved_at,rejected_at,role&order=created_at.asc', { fresh: true, headers: headers(accessToken) });
+}
+
+export function approveProfile(accessToken, id) {
+  return supabaseRequest('/rest/v1/rpc/approve_profile', { method: 'POST', fresh: true, headers: headers(accessToken), body: { target_profile_id: id } });
+}
+
+export function rejectProfile(accessToken, id) {
+  return supabaseRequest('/rest/v1/rpc/reject_profile', { method: 'POST', fresh: true, headers: headers(accessToken), body: { target_profile_id: id } });
+}
